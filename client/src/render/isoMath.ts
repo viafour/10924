488a15dd98d76189f4e43e9ec628b758999d66a1
Nathan Kaylor <115372292+viafour@users.noreply.github.com>
2026-05-09 13:03:16 -0600

    Add isometric screen to grid conversion

--- client/src/render/isoMath.ts
+++ client/src/render/isoMath.ts
@@ -8,12 +8,19 @@ export type ScreenPoint = {
   y: number;
 };
 
-const tileWidth = 96;
-const tileHeight = 48;
+export const tileWidth = 96;
+export const tileHeight = 48;
 
 export function gridToScreen(point: GridPoint): ScreenPoint {
   return {
     x: (point.x - point.y) * (tileWidth / 2),
     y: (point.x + point.y) * (tileHeight / 2)
+  };
+}
+
+export function screenToGrid(point: ScreenPoint): GridPoint {
+  return {
+    x: point.y / tileHeight + point.x / tileWidth,
+    y: point.y / tileHeight - point.x / tileWidth
   };
 }
